374722d63952c14c2f69bd1ebbc0ff8c4ff22077
vagrant <vagrant@vagrant.vm>
2019-05-14 13:46:17 +0000

    Add models

--- app/Agent.php
+++ app/Agent.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agent extends Model
 {
-    //
+    //fields
+    public $username
 }
--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    //fields
+    public $bookingId
+    public $travelerFullname
+    public $museumBooked
+    public $date
 }

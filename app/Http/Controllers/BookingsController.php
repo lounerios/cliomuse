b1de8a5ccc3f8a863dbbf4bcacf3add2c48f4872
vagrant <vagrant@vagrant.vm>
2019-05-15 13:46:42 +0000

    Add check for agent

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Booking;
+use Illuminate\Support\Facades\Log;
 
 class BookingsController extends Controller
 {
     function view() {
         $agent = session('agent');
+
+        if(strlen($agent) == 0) {
+            return redirect('/');
+        }
+
+        Log::info($agent);
 
         $bookings = Booking::GetBookingByAgentName($agent);
 
